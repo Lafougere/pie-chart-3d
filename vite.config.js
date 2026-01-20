2163860415804611005d62bf152a215a823fe6d0
Axel Glorieux <axel.glorieux@gmail.com>
2026-01-20 18:47:29 +0100

    🔧 split deps

--- vite.config.js
+++ vite.config.js
@@ -25,43 +25,43 @@ export default defineConfig({
 			// 	moduleSideEffects: false,
 			// },
 			// treeshake: 'smallest',
-			// output: {
-			// 	manualChunks: {
-			// 		// 'three-addons': ['three/addons'],
-			// 		// // 'three-examples': ['three/examples/jsm'],
-			// 		// three: ['three'],
-			// 		// tweenjs: ['@tweenjs/tween.js'],
-			// 		// lit: ['lit'],
-			// 	},
-			// },
-		},
-	},
-	test: {
-		projects: [
-			{
-				extends: true,
-				plugins: [
-					// The plugin will run tests for the stories defined in your Storybook config
-					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-					storybookTest({
-						configDir: path.join(dirname, '.storybook'),
-					}),
-				],
-				test: {
-					name: 'storybook',
-					browser: {
-						enabled: true,
-						headless: true,
-						provider: playwright({}),
-						instances: [
-							{
-								browser: 'chromium',
-							},
-						],
-					},
-					setupFiles: ['.storybook/vitest.setup.ts'],
+			output: {
+				manualChunks: {
+					'three-addons': ['three/addons'],
+					// // 'three-examples': ['three/examples/jsm'],
+					three: ['three'],
+					tweenjs: ['@tweenjs/tween.js'],
+					lit: ['lit'],
 				},
 			},
-		],
+		},
 	},
+	// test: {
+	// 	projects: [
+	// 		{
+	// 			extends: true,
+	// 			plugins: [
+	// 				// The plugin will run tests for the stories defined in your Storybook config
+	// 				// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+	// 				storybookTest({
+	// 					configDir: path.join(dirname, '.storybook'),
+	// 				}),
+	// 			],
+	// 			test: {
+	// 				name: 'storybook',
+	// 				browser: {
+	// 					enabled: true,
+	// 					headless: true,
+	// 					provider: playwright({}),
+	// 					instances: [
+	// 						{
+	// 							browser: 'chromium',
+	// 						},
+	// 					],
+	// 				},
+	// 				setupFiles: ['.storybook/vitest.setup.ts'],
+	// 			},
+	// 		},
+	// 	],
+	// },
 })
